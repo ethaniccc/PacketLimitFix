bef6d46ddaae78afa3c6d3c3ee2cb9afa8be9ece
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2022-12-18 17:39:25 +0900

    support pm4

--- src/ethaniccc/PacketLimitFix/Main.php
+++ src/ethaniccc/PacketLimitFix/Main.php
@@ -4,17 +4,23 @@ declare(strict_types=1);
 
 namespace ethaniccc\PacketLimitFix;
 
-use pocketmine\network\mcpe\RakLibInterface;
+use pocketmine\event\Listener;
+use pocketmine\event\server\NetworkInterfaceRegisterEvent;
+use pocketmine\network\mcpe\raklib\RakLibInterface;
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase{
+class Main extends PluginBase implements Listener{
 
-    public function onEnable(){
-        foreach($this->getServer()->getNetwork()->getInterfaces() as $interface){
-            if($interface instanceof RakLibInterface){
-                $interface->setPacketLimit(PHP_INT_MAX);
-            }
+    public function onEnable(): void{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function NetworkInterfaceRegister(NetworkInterfaceRegisterEvent $event) : void{
+        $raknetInterface = $event->getInterface();
+        if(!$raknetInterface instanceof RakLibInterface){
+          return;
         }
+        $raknetInterface->setPacketLimit(PHP_INT_MAX);
     }
 
 }
